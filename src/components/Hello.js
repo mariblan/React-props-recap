42a899b765efdff8dfd5e6ecd9c8b181818800ff
mariblan <99274158+mariblan@users.noreply.github.com>
2023-02-01 13:07:53 +0100

    step4

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -6,21 +6,35 @@ import React from 'react';
 // 4. Using {} - array of objects
 // 5. Using {} - array of objects with map
 // 6. Using {} - array of objects with map and key
+// 7. Using {} - array of objects with map and key and index
+// 8. Goals of the class! - props - Codesandbox --> https://codesandbox.io/s/react-props-recap-tnw729
 
 function Hello(props) {
-    console.log(props);
+  console.log(props);
   const name = 'Harry';
   return (
-        <>
-            <div>Hello {name}</div>
-            <div>Hello {props.name}</div>
-            <div>Hello {props.person.fname} {props.person.lname}</div>
-            <div>Hello {props.persons[0].fname} {props.persons[0].lname}</div>
-            <div>Hello {props.persons[1].fname} {props.persons[1].lname}</div>
-            <div>Hello {props.persons[2].fname} {props.persons[2].lname}</div>
-            {props.persons.map((person, index) => <div key={index}>Hello {person.fname} {person.lname}</div>)}
-        </>
-  )
+    <>
+      <div>Hello {name}</div>
+      <div>Hello {props.name}</div>
+      <div>
+        Hello {props.person.fname} {props.person.lname}
+      </div>
+      <div>
+        Hello {props.persons[0].fname} {props.persons[0].lname}
+      </div>
+      <div>
+        Hello {props.persons[1].fname} {props.persons[1].lname}
+      </div>
+      <div>
+        Hello {props.persons[2].fname} {props.persons[2].lname}
+      </div>
+      {props.persons.map((person, index) => (
+        <div key={index}>
+          Hello {person.fname} {person.lname}
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default Hello;
