6c8a3fe5a663c35617a3b778f5bcfb9a9dca4eed
mariblan <99274158+mariblan@users.noreply.github.com>
2023-02-01 11:58:28 +0100

    step2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-// 1. Explain the folders and files you can delete.
-// 2. How to do a component in React. https://reactjs.org/docs/components-and-props.html
-// 3. What is JSX? https://reactjs.org/docs/introducing-jsx.html
-// 4. What is a prop? https://reactjs.org/docs/components-and-props.html
+import Hello from './components/Hello';
+// How to create a component in React
+// 1. Create a function that returns JSX
+// 2. Name the function with a capital letter
+// 3. Export the function
+// 4. Import the function into another file
+// 5. Use the function in the other file
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Hello />
     </div>
   );
 }
